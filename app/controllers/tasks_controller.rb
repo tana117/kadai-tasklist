c0bba17f1a1867f807581de9a1368649d0122d03
Hirofumi Tanahashi <hiro.tanahashi117@gmail.com>
2017-09-19 12:03:49 +0000

    add update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
-before_action :require_user_logged_in, only: [:create, :show, :edit, :destroy]
-before_action :correct_user, only: [:show, :edit, :destroy]
+before_action :require_user_logged_in, only: [:create, :show, :edit, :destroy, :update]
+before_action :correct_user, only: [:show, :edit, :destroy, :update]
 
   def index
     @tasks = current_user.tasks
